bdf7a0c5e20c2cfc540136a1ab65377469618465
Diogo Costa Santos <diogo@codigocentral.com.br>
2021-09-29 20:25:12 -0300

    comit inicial

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite um texto");
+
+            var digitadoPeloUsuario = Console.ReadKey(true);
+
+            switch (digitadoPeloUsuario.KeyChar)
+            {
+                case 'a':
+                    {
+                        Console.WriteLine($"Você digitou a");
+                        break;
+                    }
+                    case 'b':
+                    {
+                        Console.WriteLine($"Você digitou b");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine($"Você digitou {digitadoPeloUsuario.KeyChar}");
+                        break;
+                    }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
